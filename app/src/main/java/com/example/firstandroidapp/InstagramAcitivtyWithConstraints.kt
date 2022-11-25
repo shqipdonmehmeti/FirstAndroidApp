8f4e49c7ba3db11830209fe0d4f791228bf20904
Shqipdon Mehmeti <keds30671@KEDS.kv>
2022-11-25 18:47:36 +0100

    constraints

---
+++ app/src/main/java/com/example/firstandroidapp/InstagramAcitivtyWithConstraints.kt
@@ -0,0 +1,11 @@
+package com.example.firstandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstagramAcitivtyWithConstraints : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instagram_acitivty_with_constraints)
+    }
+}
--- app/src/main/java/com/example/firstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.firstandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val btnCreateToast : AppCompatButton = findViewById(R.id.btnCreateToast)
+//        btnCreateToast.setOnClickListener {
+//            var toast = Toast.makeText(this,
+//                "This is one toast when button is clicked",
+//                Toast.LENGTH_LONG)
+//            toast.setGravity(Gravity.CENTER,0,0)
+//            toast.show()
+//
+//        }
     }
 }
