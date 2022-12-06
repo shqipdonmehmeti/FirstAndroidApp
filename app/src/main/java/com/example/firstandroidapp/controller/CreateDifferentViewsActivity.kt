eddce2f28bd89b472efc6fbc86a3120b5a129194
Shqipdon Mehmeti <keds30671@KEDS.kv>
2022-12-06 18:33:17 +0100

    create different views

--- app/src/main/java/com/example/firstandroidapp/controller/CreateDifferentViewsActivity.kt
+++ app/src/main/java/com/example/firstandroidapp/controller/CreateDifferentViewsActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstandroidapp.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.firstandroidapp.R
 import com.example.firstandroidapp.databinding.ActivityCreateDifferentViewsBinding
 import com.google.android.material.snackbar.Snackbar
@@ -14,10 +15,36 @@ class CreateDifferentViewsActivity : AppCompatActivity() {
         binding = ActivityCreateDifferentViewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         createToast()
-        createSnackBar()
+        createSnackBarOnButtonClick()
+        createAlertDialog()
     }
 
-    private fun createSnackBar() {
+    private fun createAlertDialog() {
+        binding.btnCreateAlertDialog.setOnClickListener {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("First Alert Dialog Title")
+            alertDialogBuilder.setMessage("First Alert Dialog Message goes right here")
+            alertDialogBuilder.setPositiveButton("OK") { dialog , which ->
+                createSnackBar("Keni klikuar ne butonin OK")
+            }
+
+            alertDialogBuilder.setNeutralButton("I'm not sure") {dialog , which ->
+
+            }
+
+            alertDialogBuilder.setNegativeButton("Cancel") {dialog , which ->
+                createSnackBar("Keni klikuar ne butonin Cancel")
+
+            }
+            alertDialogBuilder.show()
+        }
+    }
+
+    private fun createSnackBar(value : String) {
+            Snackbar.make(binding.root,value,Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun createSnackBarOnButtonClick() {
         binding.btnCreateSnackBar.setOnClickListener {
             Snackbar.make(binding.root,"Ketu kemi krijuar nje Snackbar.",Snackbar.LENGTH_LONG).show()
         }
